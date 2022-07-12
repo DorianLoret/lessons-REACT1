f48281327f653d0e57ec93eeda34aa871cb1693a
Dorian Loret <dorian.loret20@gmail.com>
2022-07-12 23:45:43 +0200

    medium

---
+++ src/pages/About.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id vitae
+        assumenda eaque aperiam nemo blanditiis commodi neque. Quisquam unde
+        mollitia nam sint, molestiae facere tenetur quis perferendis neque
+        aliquam eveniet, dolorum perspiciatis optio illo provident vel sapiente
+        doloremque quos reprehenderit harum nihil? Quidem vero voluptate, illo
+        doloribus unde architecto quaerat natus aut quam culpa error! Corporis
+        nisi deleniti autem alias, error ab, enim incidunt, veritatis modi nobis
+        doloremque? Nisi veritatis deserunt recusandae fuga voluptate earum,
+        delectus alias animi autem? Officiis, soluta. Maiores esse voluptatibus
+        nisi, ipsum aliquam, dolores ullam quia sequi consectetur voluptatem
+        ipsam repellat harum odit, perferendis dolorum nam?
+        <br />
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos optio,
+          ipsa, vitae ab deleniti animi, voluptate sequi hic omnis aspernatur
+          aliquam voluptatibus? Aliquam dolor iure et! Consectetur earum fugit
+          rem minus voluptates vero, excepturi porro ab praesentium
+          necessitatibus quam eligendi nulla temporibus magni, sed laudantium!
+          Esse magni repellendus, vitae blanditiis autem doloremque aut nihil
+          atque, quidem totam vel! Quisquam culpa molestias quia, impedit
+          exercitationem eos aliquam vitae eius, doloremque sequi optio id magni
+          molestiae itaque deleniti aspernatur, tempore tempora laborum adipisci
+          beatae sit nostrum possimus quo eum. Quos praesentium est deleniti vel
+          labore nostrum eligendi magni molestiae aperiam enim iste, voluptas
+          harum ex expedita, modi explicabo pariatur. Saepe assumenda adipisci
+          suscipit quia? Sapiente voluptate molestias minima in laborum veniam
+          architecto, hic nesciunt, consequuntur reiciendis facilis adipisci
+          esse delectus eos magnam magni labore quidem blanditiis! Aliquid autem
+          fugiat unde, doloribus dignissimos error vel ad itaque aliquam
+          quibusdam enim iure, natus, iusto sequi quas? Reiciendis fugit
+          possimus dolore a, dolor cupiditate deleniti, provident animi
+          voluptates et explicabo corrupti voluptatem nisi quos veniam
+          distinctio repudiandae earum iste beatae doloribus? Id, soluta
+          corporis. Hic minima voluptas expedita deserunt quae asperiores dolore
+          nesciunt eligendi commodi maxime distinctio quisquam mollitia,
+          accusantium iste corporis. Ex, aliquam aperiam.
+        </p>
+      </p>
+    </div>
+  );
+};
+
+export default About;
